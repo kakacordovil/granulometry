890f37f44d3bda09755028360a7d962a539ff534
Karllana Cordovil <karllanacordovil@oryon.solutions>
2023-03-06 05:00:20 +0000

    created granulometry relayCommand

---
+++ Test Granulometry/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Test_Granulometry
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
